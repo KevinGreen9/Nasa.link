52a6ba58b3ec889369e019082cd23e16f0552c34
Golden_boy <cema.56.s@mail.ru>
2024-08-30 20:26:02 +0300

    message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = (props) => {
             <div className='app-wrapper-content'>
                 <Routes>
                     <Route path='/dialogs' element={<Dialogs
-                        state={props.state.dialogsPage}/>}/>
+                        store={props.store}/>}/>
                     <Route path='/profile' element={<Profile
                         profilePage={props.state.profilePage}
                         dispatch={props.dispatch}/>}/>
