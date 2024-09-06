3f45f0b7d8d3734a98205dbf57f3057ae5d63890
Golden_boy <cema.56.s@mail.ru>
2024-09-06 11:02:22 +0300

    redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import reportWebVitals from './reportWebVitals';
-import store from "./Redux/store";
+import store from "./Redux/redux-store";
 import ReactDOM from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
 import React from "react";
@@ -19,7 +19,10 @@ let rerenderEntireTree = (state) => {
 
 
 rerenderEntireTree(store.getState());
-store.subscribe(rerenderEntireTree);
+store.subscribe(() => {
+    let state = store.getState();
+    rerenderEntireTree(state);
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
