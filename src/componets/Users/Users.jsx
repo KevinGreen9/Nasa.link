1836e50376d8988fefa29c4d9a622db4e3c93562
Golden_boy <cema.56.s@mail.ru>
2024-09-12 14:28:18 +0300

    componentDidMount

--- src/componets/Users/Users.jsx
+++ src/componets/Users/Users.jsx
@@ -1,20 +1,46 @@
 import React from 'react';
 import styles from '../../users.module.css';
-import  axios from 'axios';
+import axios from 'axios';
 import userPhoto from '../../assets/images/images.png'
 
 class Users extends React.Component {
 
-componentDidMount() {
-    axios.get("https://social-network.samuraijs.com/api/1.0/users")
-        .then(response => {
-            this.props.setUsers(response.data.items);
-        });
-}
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            })
+
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
+    }
 
     render() {
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
         return <div>
-            <button onClick={this.setUsers}>Get users</button>
+            <div>
+                {pages.map(p => {
+                    return <span className={this.props.currentPage === p ? styles.selectedPage : ''}
+                                 onClick={() => {
+                                     this.onPageChanged(p);
+                                 }}>{p}</span>
+                })}
+
+            </div>
             {
                 this.props.users && this.props.users.map(u => <div key={u.id}>
                 <span>
@@ -43,8 +69,7 @@ componentDidMount() {
                 )}
         </div>
     }
-    }
-
+}
 
 
 export default Users;
